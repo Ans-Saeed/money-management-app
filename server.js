e6d064b75efaf4164e63b34757c9a39a538fa1a4
Shanto <rakibulhasansanto25@gmail.com>
2021-07-04 00:57:18 +0600

    server setup

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(morgan('dev'))
 app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -15,6 +21,11 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log('SERVER IS RUNNONG ON PORT ${PORT}')
+    mongoose.connect(`mongodb://localhost/money-management-app`,
+        { useNewUrlParser: true },
+        () => {
+            console.log('Database Connected...')
+        });
 })
 
 
